2485dabaf518c67e69e9ee4093582c325160a092
John Matthews <johnmatthews2634@gmail.com>
2018-09-25 17:10:43 -0700

    .

--- app/controllers/api/companies_controller.rb
+++ app/controllers/api/companies_controller.rb
@@ -8,6 +8,7 @@ class Api::CompaniesController < ApplicationController
     end
   end
   private
+  # 
 
   def company_params
     params.require(:company).permit(:company_name, :payment_status, :primary_email, :primary_contact_name, :primary_phone_number)
